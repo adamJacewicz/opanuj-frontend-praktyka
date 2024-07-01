114cfdb721ed8268965d38b1470ead05f086313d
adam <adam.jacewicz4@gmail.com>
2024-07-01 21:53:03 +0200

    - shop-context refactor

--- examples/module1/lesson3/shop-context/components/Product.tsx
+++ examples/module1/lesson3/shop-context/components/Product.tsx
@@ -7,8 +7,6 @@ import { CartContext } from '../contexts/CartContext';
 import { Product as ProductType } from '../types/Product';
 
 const Product = ({ product }: { product: ProductType }) => {
-  const { addToCart } = useContext(CartContext);
-
   const { id, image, category, title, price } = product;
 
   return (
@@ -24,11 +22,7 @@ const Product = ({ product }: { product: ProductType }) => {
           </div>
         </div>
         <div className="absolute top-6 -right-11 group-hover:right-5 p-2 flex flex-col justify-center items-center gap-y-2 opacity-0 group-hover:opacity-100 transition-all duration-300">
-          <button onClick={() => addToCart(product)}>
-            <div className="flex justify-center items-center text-white w-12 h-12 bg-teal-500">
-              <BsPlus className="text-3xl" />
-            </div>
-          </button>
+          <AddProductButton product={product} />
           <Link
             to={`/product/${id}`}
             className="w-12 h-12 bg-white flex justify-center items-center text-primary drop-shadow-xl"
@@ -45,6 +39,18 @@ const Product = ({ product }: { product: ProductType }) => {
         <h2 className="font-semibbold">$ {price}</h2>
       </div>
     </div>
+  );
+};
+
+const AddProductButton = ({ product }: { product: ProductType }) => {
+  const { addToCart } = useContext(CartContext);
+
+  return (
+    <button onClick={() => addToCart(product)}>
+      <div className="flex justify-center items-center text-white w-12 h-12 bg-teal-500">
+        <BsPlus className="text-3xl" />
+      </div>
+    </button>
   );
 };
 
